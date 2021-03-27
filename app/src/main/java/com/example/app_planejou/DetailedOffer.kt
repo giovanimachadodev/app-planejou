10c410a1106918a0f53bdc08c2efc9dc6548782f
Giovani Machado <giovanimachadodev@gmail.com>
2021-03-27 11:50:00 -0300

    add screens

---
+++ app/src/main/java/com/example/app_planejou/DetailedOffer.kt
@@ -0,0 +1,11 @@
+package com.example.app_planejou
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailedOffer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detailed_offer)
+    }
+}
---
+++ app/src/main/java/com/example/app_planejou/FilterServices.kt
@@ -0,0 +1,11 @@
+package com.example.app_planejou
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FilterServices : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_filter_services)
+    }
+}
