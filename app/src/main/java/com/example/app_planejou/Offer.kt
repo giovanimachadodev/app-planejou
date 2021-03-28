22612ce3fec7636f672bce8a2ff35374049d389e
Giovani Machado <giovanimachadodev@gmail.com>
2021-03-28 12:16:31 -0300

    Add Adapter Offers

--- app/src/main/java/com/example/app_planejou/Offer.kt
+++ app/src/main/java/com/example/app_planejou/Offer.kt
@@ -2,10 +2,30 @@ package com.example.app_planejou
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class Offer : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_offer)
+
+        val rvOffer = findViewById<RecyclerView>(R.id.rvOffer)
+        val adapterProdutos = OfferAdapter(listOffer)
+        rvOffer.adapter = adapterProdutos
+
+
+        rvOffer.layoutManager = LinearLayoutManager(this)
+
+    }
+
+    companion object{
+        val listOffer = mutableListOf(
+            Offers( R.drawable.finlandia, "Teste", 2000.0 ),
+            Offers( R.drawable.finlandia, "Teste", 2000.0 ),
+            Offers( R.drawable.finlandia, "Teste", 2000.0 )
+        )
     }
 }
+
+
---
+++ app/src/main/java/com/example/app_planejou/Offers.kt
@@ -0,0 +1,6 @@
+package com.example.app_planejou
+
+import android.widget.ImageView
+
+class Offers(val image: Int, val details: String, val price: Double ) {
+}
