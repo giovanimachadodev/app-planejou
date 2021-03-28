a360744738090d4e9269091cda4fa96539410804
Jessica <j.zaparolli.arq@gmail.com>
2021-03-27 22:56:08 -0300

    splashScreen

--- app/src/main/java/com/example/app_planejou/SplashScreen.kt
+++ app/src/main/java/com/example/app_planejou/SplashScreen.kt
@@ -1,11 +1,25 @@
 package com.example.app_planejou
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_splash_screen)
+        splash()
     }
+
+    fun splash() {
+        Handler(
+            Looper.getMainLooper()).postDelayed({
+            val proximaTela =
+                Intent(this, MainActivity::class.java)
+            startActivity(proximaTela)
+            finish()
+        }, 3000)}
 }
